cc86d11a127114fd31a19ca771cbe3a4b2e9a83b
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-23 13:50:09 +0100

    feat: exercise + bonus completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+  display: flex;
+  align-items: center;
+  background-color: lightgray;
+  height: 500px;
+  border-radius: 30px;
+  margin: 2em;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite! :)",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style>
+h1 {
+  color: black;
+  text-align: center;
+  padding-top: 1em;
+  position: absolute;
+  top: 200px;
+  right: 0;
+  left: 0;
+}
+</style>
